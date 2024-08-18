482642631739302436e079ffd951caaa6384ddc0
Nauman <inaumanmajeed@gmail.com>
2024-08-19 00:19:55 +0500

    chore: Update import paths and styles for Home component and BasicInfoCard

--- src/app/components/home/userInputcards/BasicInfoCard.js
+++ src/app/components/home/userInputcards/BasicInfoCard.js
@@ -3,31 +3,34 @@ import UserInputFormLayoutCard from "app/layouts/userInputCards";
 import BasicImg from "app/assets/images/Home/basicInfo.svg";
 import { Field } from "formik";
 import CustomInput from "app/components/shared/input/Input";
+import { FormContainer } from "../styles";
 
 const BasicInfoCard = () => {
   return (
     <UserInputFormLayoutCard img={BasicImg} title={"Basic Information"}>
-      <Field
-        name="firstName"
-        type="text"
-        label="First Name"
-        placeholder="Enter your first name"
-        component={CustomInput} // Use CustomInput here
-      />
-      <Field
-        name="lastName"
-        type="text"
-        label="Last Name"
-        placeholder="Enter your last name"
-        component={CustomInput} // Use CustomInput here
-      />
-      <Field
-        name="email"
-        type="email"
-        label="Email"
-        placeholder="Enter your email"
-        component={CustomInput} // Use CustomInput here
-      />
+      <FormContainer>
+        <Field
+          name="firstName"
+          type="text"
+          label="First Name"
+          placeholder="Enter your first name"
+          component={CustomInput}
+        />
+        <Field
+          name="lastName"
+          type="text"
+          label="Last Name"
+          placeholder="Enter your last name"
+          component={CustomInput}
+        />
+        <Field
+          name="email"
+          type="email"
+          label="Email"
+          placeholder="Enter your email"
+          component={CustomInput}
+        />
+      </FormContainer>
     </UserInputFormLayoutCard>
   );
 };
--- src/app/components/shared/input/Input.js
+++ src/app/components/shared/input/Input.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { CustomInputMainContainer } from "./styles";
 
 const CustomInput = ({ field, form, type = "text", label, ...props }) => {
   return (
-    <div>
+    <CustomInputMainContainer>
       <label>{label}</label>
       <input {...field} type={type} {...props} />
       {form.errors[field.name] && form.touched[field.name] ? (
-        <div style={{ color: "red" }}>{form.errors[field.name]}</div>
+        <div className="error">{form.errors[field.name]}</div>
       ) : null}
-    </div>
+    </CustomInputMainContainer>
   );
 };
 
